c0e803fac1da25f3f287006a9e294004356f1887
Lakshya Jain <86834335+Lakshya2578@users.noreply.github.com>
2024-05-27 13:10:46 +0530

    Update pizza sales analysis.sql

--- pizza sales analysis.sql
+++ pizza sales analysis.sql
@@ -114,3 +114,17 @@ on orders_details.pizza_id = pizzas.pizza_id
 group by pizza_types.category order by revenue desc ;
 
 -- ____________________________________________________________________________________________________________________________ 
+-- Analyze the cumulative revenue generated over time. 
+
+select order_date,
+sum(revenue) over(order by order_date) as cum_revenue
+from
+(select orders.order_date,
+sum(orders_details.quantity * pizzas.price) as revenue
+from orders_details join pizzas
+on orders_details.pizza_id = pizzas.pizza_id
+join orders
+on orders.order_id = orders_details.order_id
+group by orders.order_date) as sales;
+
+-- ______________________________________________________________________________________________________________________________ 
